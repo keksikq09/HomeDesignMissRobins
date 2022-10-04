6814763a4f91762f91827e436fb885605d4203dd
keksikq09 <nikitatabor187@gmail.com>
2022-10-04 13:47:57 +0300

    Init

---
+++ Assets/Scripts/DigitalRuby/ThunderAndLightning/LightningBoltPrefabScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace DigitalRuby.ThunderAndLightning
+{
+	public class LightningBoltPrefabScript : LightningBoltPrefabScriptBase
+	{
+		public GameObject Source;
+
+		public GameObject Destination;
+
+		public Vector3 StartVariance;
+
+		public Vector3 EndVariance;
+
+		public override void CreateLightningBolt(LightningBoltParameters parameters)
+		{
+			parameters.Start = ((Source == null) ? parameters.Start : Source.transform.position);
+			parameters.End = ((Destination == null) ? parameters.End : Destination.transform.position);
+			parameters.StartVariance = StartVariance;
+			parameters.EndVariance = EndVariance;
+			base.CreateLightningBolt(parameters);
+		}
+	}
+}
